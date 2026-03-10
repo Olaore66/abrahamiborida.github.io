cdbe1994d3b897af545f046bcf399715b9e4dddf
olaore66 <olaore66@gmail.com>
2026-03-10 11:21:21 +0100

    Updates

--- _next/static/xoC-lK0RAHy-3rMsK8DSp/_buildManifest.js
+++ _next/static/xoC-lK0RAHy-3rMsK8DSp/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/abrahamiborida.github.io//_next/:path+",
+        "destination": "/abrahamiborida.github.io/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
